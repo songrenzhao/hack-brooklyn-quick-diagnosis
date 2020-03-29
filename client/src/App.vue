cf79626fe1422351c4628c727eb1aa0093c82c0b
ThisZw <zhiweichrishuang@gmail.com>
2020-03-29 15:28:59 -0400

    setting up main page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+    <v-app id="">
+      <v-content>
+        <v-container
+          class="fill-height"
+          id="main-container"
+          fluid
+        >
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-col>
+              <v-card class="elevation-12">
+                <router-view id=""/>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  #main-container {
+    max-width: 960px;
+    margin: auto;
+  }
 </style>
---
+++ client/src/components/Search.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-toolbar
+      color="primary"
+      dark
+      flat
+    >
+      <v-toolbar-title>{{ msg }}</v-toolbar-title>
+      <v-spacer />
+    </v-toolbar>
+
+    <router-view id="main"/>
+
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          label="Tell me what are you feeling wrong today."
+          name="search"
+          prepend-icon="local_hospital"
+          type="text"
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary">Login</v-btn>
+    </v-card-actions>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Search extends Vue {
+  @Prop() msg !: string;
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
